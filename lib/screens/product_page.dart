21b088efc46fbf7ad25e5785cdc924b8a9b49740
georgioadwan <gadwan@student.uls.edu.lb>
2022-07-03 21:07:31 +0300

    Product page almost done with issues from firebase "_Json document to subtype Map<String, dynamic>"

--- lib/screens/product_page.dart
+++ lib/screens/product_page.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:final_project/constants.dart';
 import 'package:final_project/widgets/custom_action_bar.dart';
 import 'package:final_project/widgets/images_swipe.dart';
+import 'package:final_project/widgets/painting_size.dart';
 import 'package:flutter/material.dart';
 
 class ProductPage extends StatefulWidget {
@@ -14,17 +15,16 @@ class ProductPage extends StatefulWidget {
 
 class _ProductPageState extends State<ProductPage> {
   final CollectionReference<Map<String, dynamic>> _productsRef =
-  FirebaseFirestore.instance.collection("Paintings");
-
+      FirebaseFirestore.instance.collection("Paintings");
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack (
-        children: [
-          FutureBuilder(
+        body: Stack(
+      children: [
+        FutureBuilder(
             future: _productsRef.doc(widget.productId).get(),
-            builder: (context,snapshot) {
+            builder: (context, snapshot) {
               if (snapshot.hasError) {
                 return Scaffold(
                   body: Center(
@@ -35,14 +35,18 @@ class _ProductPageState extends State<ProductPage> {
 
               if (snapshot.connectionState == ConnectionState.done) {
                 // firebase Document Data Map
-                Map <String, dynamic> documentData = snapshot.data!.data();
+                Map<String, dynamic> documentData =
+                    snapshot.data as Map<String, dynamic>;
 
                 // List of images
                 List imageList = documentData['images'];
+                List paintingSizes = documentData['size'];
                 return ListView(
                   padding: EdgeInsets.all(0),
                   children: [
-                    ImageSwipe(imageList: [imageList],),
+                    ImageSwipe(
+                      imageList: [imageList],
+                    ),
                     Padding(
                       padding: const EdgeInsets.symmetric(
                         vertical: 4.0,
@@ -50,7 +54,7 @@ class _ProductPageState extends State<ProductPage> {
                       ),
                       child: Text(
                         "${documentData['name']}",
-                      style: Constants.boldHeading,
+                        style: Constants.boldHeading,
                       ),
                     ),
                     Padding(
@@ -59,35 +63,83 @@ class _ProductPageState extends State<ProductPage> {
                         left: 24.0,
                         right: 24.0,
                         bottom: 4.0,
-              ),
+                      ),
                       child: Text(
                         "\$${documentData['price']}",
-                      style: TextStyle(
-                        fontSize: 18.0,
-                        color: Theme.of(context).accentColor,
-                        fontWeight: FontWeight.w600,
-                      ),),
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          color: Theme.of(context).accentColor,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
                     ),
                     Padding(
                       padding: const EdgeInsets.symmetric(
                         vertical: 8.0,
                         horizontal: 24.0,
                       ),
-                      child: Text (
+                      child: Text(
                         "${documentData['desc']}",
-                      style: TextStyle(
-                        fontSize: 16.0,
-                      ),),
+                        style: TextStyle(
+                          fontSize: 16.0,
+                        ),
+                      ),
                     ),
                     Padding(
                       padding: const EdgeInsets.symmetric(
                         vertical: 24.0,
                         horizontal: 24.0,
                       ),
-                      child: Text (
+                      child: Text(
                         "${documentData['size']}",
-                      style: Constants.regularDarkText,),
+                        style: Constants.regularDarkText,
+                      ),
                     ),
+                    PaintingSize(
+                      paintingSizes: paintingSizes,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(24.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Container(
+                            width: 65.0,
+                            height: 65.0,
+                            decoration: BoxDecoration(
+                              color: Color(0xFFDCDCDC),
+                              borderRadius: BorderRadius.circular(12.0),
+                            ),
+                            alignment: Alignment.center,
+                            child: Image(
+                              image: AssetImage(
+                                "assets/images/bookmark.png",
+                              ),
+                              height: 22.0,
+                            ),
+                          ),
+                          Expanded(
+                            child: Container(
+                              height: 65.0,
+                              margin: EdgeInsets.only(
+                                left: 16.0,
+                              ),
+                              decoration: BoxDecoration(
+                                color: Colors.black,
+                                borderRadius: BorderRadius.circular(12.0),
+                              ),
+                              alignment: Alignment.center,
+                              child: Text("Add to Cart",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.w600,
+                              ),),
+                            ),
+                          )
+                        ],
+                      ),
+                    )
                   ],
                 );
               }
@@ -98,15 +150,13 @@ class _ProductPageState extends State<ProductPage> {
                   child: CircularProgressIndicator(),
                 ),
               );
-            }
-          ),
-          CustomActionBar(
-            hasBackArrow: true,
-            hasTitle: false,
-            hasBackground: false,
-          )
-        ],
-      )
-    );
+            }),
+        CustomActionBar(
+          hasBackArrow: true,
+          hasTitle: false,
+          hasBackground: false,
+        )
+      ],
+    ));
   }
 }
---
+++ lib/widgets/painting_size.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class PaintingSize extends StatefulWidget {
+  final List? paintingSizes;
+  PaintingSize ({this.paintingSizes});
+
+  @override
+  State<PaintingSize> createState() => _PaintingSizeState();
+}
+
+class _PaintingSizeState extends State<PaintingSize> {
+
+  int _selected = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 20.0,
+      ),
+      child: Row(
+        children: [
+          for (var i = 0; i <widget.paintingSizes!.length; i++)
+            GestureDetector(
+              onTap: () {
+                setState ((){
+                  _selected = i;
+                });
+              },
+              child: Container(
+                width: 42.0,
+                height: 42.0,
+                decoration: BoxDecoration(
+                  color: _selected == i ? Theme.of(context).accentColor : Color(0xFFDCDCDC),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                alignment: Alignment.center,
+                margin: EdgeInsets.symmetric(
+                  horizontal: 4.0,
+                ),
+                child: Text("${widget.paintingSizes![i]}",
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  color: _selected == i ? Colors.white : Colors.black,
+                  fontSize: 16.0,
+                ),),
+              ),
+            )
+        ],
+      ),
+    );
+  }
+}
